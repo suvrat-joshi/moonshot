bad66ebdbc9f6fc289a50a0e3ccab2ce34508ffa
James Armes <james.armes@acquia.com>
2019-09-18 09:53:05 -0400

    Allow the destination for dynamic templates to be overridden via CLI. (#262)

--- lib/plugins/dynamic_template.rb
+++ lib/plugins/dynamic_template.rb
@@ -13,10 +13,22 @@ module Moonshot
         @dynamic_template.process
       end
 
+      def cli_hook(parser)
+        parser.on('-sPATH', '--destination-path=PATH',
+                  'Destination path for the dynamically generated template', String) do |value|
+          @dynamic_template.destination = value
+        end
+      end
+
       # Moonshot hooks to trigger this plugin.
       alias setup_create run_hook
       alias setup_update run_hook
       alias setup_delete run_hook
+
+      # Moonshot hooks to add CLI options.
+      alias create_cli_hook cli_hook
+      alias delete_cli_hook cli_hook
+      alias update_cli_hook cli_hook
     end
   end
 end
